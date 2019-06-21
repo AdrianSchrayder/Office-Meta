ab407f6d2908f3aa17443875f9b71e296e56ef07
Adrian <43765518+AdrianSchrayder@users.noreply.github.com>
2019-06-21 13:55:35 +0800

    Add files via upload

---
+++ typechange.js
@@ -0,0 +1,14 @@
+onSwitchIn: function (pokemon) {
+       let changed = {'Arbok':true, 'Milotic':true, 'Cherrim-Sunshine':true};
+       let bt = pokemon.baseTemplate;
+       if (bt.baseSpecies in changed || (bt.actualSpecies && bt.actualSpecies in changed)) {
+         let types = bt.types;
+         let bTypes = (types.length === 1 || types[1] === 'caw') ? types[0] : types.join('/');
+         this.add('-start', pokemon, 'typechange', bTypes, '[silent]');
+       }
+       if (bt.actualSpecies) this.add('-start', pokemon, bt.actualSpecies, '[silent]'); //Show the pokemon's actual species
+     },
+     onSwitchOut: function (pokemon) {
+       if (pokemon.baseTemplate.actualSpecies) this.add('-end', pokemon, pokemon.baseTemplate.actualSpecies, '[silent]');
+     },
+   }
