edbf43575c36270b4240e0de3decf89f376cf168
PandaGabe <51995445+PandaGabe@users.noreply.github.com>
2019-06-24 05:44:37 -0700

    Update abilities.js

--- abilities.js
+++ abilities.js
@@ -846,11 +846,11 @@ let BattleAbilities = {
 			if (move && move.flags['contact'] && !source.status && source.runStatusImmunity('powder')) {
 				let r = this.random(100);
 				if (r < 11) {
-					source.setStatus('slp', target);
+					source.setStatus('psn', target);
 				} else if (r < 21) {
 					source.setStatus('par', target);
-				} else if (r < 30) {
-					source.setStatus('psn', target);
+				} else if (r < 50) {
+					source.setStatus('slp', target);
 				}
 			}
 		},
